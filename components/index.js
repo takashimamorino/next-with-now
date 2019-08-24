8fef1829e1023361bdf98a5b5de1380857c84b05
takashimamorino <does1026@gmail.com>
2019-08-25 00:46:54 +0900

    initialize

---
+++ components/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Test = () => {
+  return (
+    <>
+      <div>Planet</div>
+      <img src="../static/planet_sample.png" alt="planet" width="500" />
+    </>
+  )
+};
+
+export default Test;
---
+++ pages/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+import Test from '../components'
+
+const Home = () => <Test />;
+
+export default Home
